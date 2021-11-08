c02e28805ab4c55ba3d480b4ee18a8d6131617b6
Tomáš Liška <76174508+kontridio@users.noreply.github.com>
2021-11-08 16:49:12 +0100

    Add files via upload

---
+++ zoom.js
@@ -0,0 +1,29 @@
+const zoom = document.getElementById("scale");
+
+zoom.addEventListener("mouseover", function () {
+  zoom.style.fontSize = "28px";
+});
+
+zoom.addEventListener("mouseout", function () {
+  zoom.style.fontSize = "25px";
+});
+
+const zoom1 = document.getElementById("scale1");
+
+zoom1.addEventListener("mouseover", function () {
+  zoom1.style.fontSize = "28px";
+});
+
+zoom1.addEventListener("mouseout", function () {
+  zoom1.style.fontSize = "25px";
+});
+
+const zoom2 = document.getElementById("scale2");
+
+zoom2.addEventListener("mouseover", function () {
+  zoom2.style.fontSize = "28px";
+});
+
+zoom2.addEventListener("mouseout", function () {
+  zoom2.style.fontSize = "25px";
+});
